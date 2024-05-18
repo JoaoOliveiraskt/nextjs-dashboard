037a24e366fdb0c28632bf82580c7705c3a00fc7
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-05-18 12:49:33 -0300

    refactor: Update font imports in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,5 @@
 import '@/app/ui/global.css';
-import { Inter, Lusitana } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-const lusitana = Lusitana({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-});
-
-export {lusitana};
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
